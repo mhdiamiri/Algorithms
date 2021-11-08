47fc5d89805dbfe568252eebc8f7ecdf5bf95116
Mehdi Amiri <officialmehdiamiri@gmail.com>
2021-11-08 08:24:42 +0330

    nov8-2

--- dynamic_programming/subset_sum1.cpp
+++ dynamic_programming/subset_sum1.cpp
@@ -2,18 +2,20 @@
 using namespace std;
 
 int main(){
+    //input:
     int n; cin >> n;
     int arr[n]; for(int i = 0; i < n; i++) cin >> arr[i];
     int k; cin >> k;
+    // base:
     set<int> ans;
     ans.insert(0);
-    
+    // steps:
     for(int i = 0; i< n;i++){
         set<int> tmp;
         for(int a: ans) tmp.insert(arr[i] + a);
         ans.insert(tmp.begin(), tmp.end());
     }
-    
+    // check if k is a member of ans or not.
     if(ans.find(k) != ans.end()) cout << "YES";
     else cout << "NO";
     return 0;
